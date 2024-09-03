bb602da658a537de38800173c4a9bdf0e00a4d78
Rodrigo Ferreira Oliveira de Paula <39167303+rfop2@users.noreply.github.com>
2024-09-03 20:33:23 -0300

    create Tarefa + disable edit data_terminio

---
+++ app/Filament/Resources/TarefaResource/Pages/CreateTarefa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\TarefaResource\Pages;
+
+use App\Filament\Resources\TarefaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTarefa extends CreateRecord
+{
+    protected static string $resource = TarefaResource::class;
+}
